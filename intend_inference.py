3114ee0567016ba560a60816337b1ceb81a5ece3
Tingyi Wanyan <tingyiwanyan@osph-570343.dhcp.swmed.org>
2023-07-04 19:41:42 -0500

    correction

--- intend_inference.py
+++ intend_inference.py
@@ -95,8 +95,10 @@ def compute_metrics(eval_pred):
 
 	accuracy = evaluate.load("accuracy")
 	predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return accuracy.compute(predictions=predictions, references=labels)
+	
+	predictions = np.argmax(predictions, axis=1)
+
+	return accuracy.compute(predictions=predictions, references=labels)
     
 
 
